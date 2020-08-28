09993b9ebf250f31b9f2332377875230a2dda1f2
NALLEIN <nigelts@sjtu.edu.cn>
2020-08-28 18:14:01 +0800

    Add layer test

--- modules/js/test/test_dnn.js
+++ modules/js/test/test_dnn.js
@@ -69,16 +69,7 @@ function asyncForwardWrapper(net) {
     });
 }
 
-QUnit.test('test_layer_softmax', async function(assert) {
-    const adapter = await navigator.gpu.requestAdapter();
-    const device = await adapter.requestDevice();
-    Module.preinitializedWebGPUDevice = device;
-    const layer_softmax = "https://raw.githubusercontent.com/opencv/opencv_extra/master/testdata/dnn/layers/layer_softmax.prototxt";
-    const inputSize = [2, 6, 75, 113];
-    const url = {
-        modelUrl: layer_softmax,
-        configUrl: ""
-    }; 
+async function loadAndComputeCaffeLayer(url, inputSize) {
     const path = await loadModel(url);
     let net, net1;
     const input = new cv.Mat(inputSize, cv.CV_32F);
@@ -87,31 +78,49 @@ QUnit.test('test_layer_softmax', async function(assert) {
     net.setInput(input);
     net.setPreferableBackend(cv.DNN_BACKEND_WGPU);
     net.setPreferableTarget(cv.DNN_TARGET_WGPU);
-
     const start = performance.now();
-    // const out = net.forward();
     const out = await asyncForwardWrapper(net);
     const time = (performance.now() - start).toFixed(3);
     console.log(`WebGPU backend time cost ${time} ms.`);
 
     net1 = cv.readNetFromCaffe(path.modelPath, '');
     net1.setInput(input);
-    net1.setPreferableBackend(cv.DNN_BACKEND_DEFAULT);
-    net1.setPreferableTarget(cv.DNN_TARGET_CPU);
-
     const start1 = performance.now();
     const out1 = net1.forward();
     const time1 =(performance.now() - start1).toFixed(3);
     console.log(`CPU backend time cost ${time1} ms.`);
 
-    console.log(out.data.slice(0, 10));
-    console.log(out1.data.slice(0, 10));
-    assert.equal(out.data.slice(0,10), out1.data.slice(0, 10));
-    // assert.equal(out.data, out1.data);
-
     input.delete();
     net.delete();
     net1.delete();
+    return [out, out1];
+}
+
+QUnit.test('test_layer_softmax', async function(assert) {
+    const adapter = await navigator.gpu.requestAdapter();
+    const device = await adapter.requestDevice();
+    Module.preinitializedWebGPUDevice = device;
+    const layer_lrn_spatial = "https://raw.githubusercontent.com/opencv/opencv_extra/master/testdata/dnn/layers/layer_softmax.prototxt";
+    const inputSize = [2, 6, 75, 113];
+    const url = {
+        modelUrl: layer_lrn_spatial,
+        configUrl: ""
+    }; 
+    const [out, out1] = await loadAndComputeCaffeLayer(url, inputSize);
+
+    const EPSILON = 1;
+    const data1 = out.data;
+    const data2 = out1.data;
+    assert.equal(data1.length, data2.length);
+    let absSum = 0.0;
+    const len = data1.length;
+    for(let i = 0; i < len; i++)
+    {
+        absSum += Math.abs(data1[i] - data2[i]) / len;
+    }
+    console.log(`L1 norm: ${absSum}`);
+    assert.ok(absSum < EPSILON);
+
     out.delete();
     out1.delete();
 });
